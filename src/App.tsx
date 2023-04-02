59a025b14a974c011d78902d2729bc667ad840b9
Rishab Gupta <rishabgupta98@gmail.com>
2023-04-02 19:18:18 -0300

    adds google login

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,33 @@
+import firebase from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [auth, setAuth] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth();
+    auth.onAuthStateChanged((user) => {
+      if (user) setAuth(true);
+    });
+  }, []);
+
+  const loginWithFirebase = () => {
+    const auth = getAuth();
+    const provider = new GoogleAuthProvider();
+    signInWithPopup(auth, provider).then((userCred) => {
+      if (userCred) setAuth(true);
+    });
+  };
+
   return (
-    <div className="App">
-      <h2>Hello World</h2>
-    </div>
+    <>
+      {auth ? (
+        <h1>Expense Tracker</h1>
+      ) : (
+        <button onClick={loginWithFirebase}>Login with Google</button>
+      )}
+    </>
   );
 }
 
