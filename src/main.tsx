383bd40fb5f76e958cba9c56576ce08be730d285
Ninh K <ninh.bkhuong@gmail.com>
2023-04-07 13:12:57 -0400

    adds signup page materialUI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+//import App from "./App";
+import Signup from "./components/Signup";
 import "./config/firebase-config";
+import "bootstrap/dist/css/bootstrap-grid.min.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Signup />
   </React.StrictMode>
 );
