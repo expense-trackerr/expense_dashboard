3533ddb792bbfea9f9fffba3b3410e273095e336
Rishab Gupta <rishabgupta98@gmail.com>
2023-04-11 23:52:00 -0300

    removes import

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Login } from "./pages/Login";
 import { AuthProvider } from "./contexts/AuthContext";
 import { Routes, Route } from "react-router-dom";
 import { Signup } from "./pages/Signup";
-import { Dashboard } from "./pages/Dashboard";
 
 function App() {
   return (
