7a847a5edb861dc1ddab26c719deb225102686af
Rishab Gupta <rishabgupta98@gmail.com>
2023-06-20 23:13:07 -0400

    uses token from localstorage

--- src/components/ListOfTodos.tsx
+++ src/components/ListOfTodos.tsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { getAuth, signOut } from 'firebase/auth';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
 import { AddCategories } from './AddCategories';
 
 type TodoData = {
@@ -16,7 +15,7 @@ type TodoData = {
 
 export function ListOfTodos() {
   const [data, setData] = useState<TodoData['data']['todos']>([]);
-  const { token } = useAuth();
+  const token = localStorage.getItem('token');
   const navigate = useNavigate();
 
   useEffect(() => {
