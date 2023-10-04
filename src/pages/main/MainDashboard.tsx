fd283f2aafcebc34d35b68756e165719d4f81ea2
Rishab Gupta <rishabgupta98@gmail.com>
2023-10-03 23:19:53 -0300

    install decimal js

--- src/pages/main/MainDashboard.tsx
+++ src/pages/main/MainDashboard.tsx
@@ -1,7 +1,29 @@
 import { Grid, Typography } from '@mui/material';
+import { useContext, useEffect } from 'react';
 import { DateRangePicker } from '../../components/DateRangePicker';
+import defaultAxios from '../../config/axiosConfig';
+import { PlaidContext } from '../../contexts/PlaidContext';
 
 export const MainDashboard = () => {
+  const { linkedAccounts } = useContext(PlaidContext);
+
+  useEffect(() => {
+    if (linkedAccounts?.length) {
+      getInitialTransactions();
+    }
+  }, [linkedAccounts]);
+
+  const getInitialTransactions = async () => {
+    const itemId = linkedAccounts?.[0].item_id;
+    try {
+      const result = await defaultAxios.get(`http://localhost:3000/api/transactions/${itemId}`);
+
+      console.log('result:', result);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <Grid container justifyContent="space-between" alignItems="center">
       <Grid item>
